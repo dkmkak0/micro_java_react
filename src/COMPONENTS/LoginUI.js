2b8bd4bf29106a33adf09f273918028272da7169
PhuocHau <phuochaupro123@gmail.com>
2025-02-15 23:29:56 +0700

    Update LoginUI

--- src/COMPONENTS/LoginUI.js
+++ src/COMPONENTS/LoginUI.js
@@ -1,42 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../STYLES/LoginUI.module.css";
-import { FaGoogle, FaFacebook } from "react-icons/fa";
+import { FaFacebook, FaEye, FaEyeSlash } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 
 const LoginUI = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
     <div className={styles.container}>
-      <h3 className={styles.welcomeText}>WELCOME TO<br /><b>TIỆM RỬA XE</b></h3>
+      <h3 className={styles.welcomeText}>
+        W E L C O M E &nbsp;&nbsp;T O
+        <br />
+        <br />
+        ATH&nbsp;&nbsp; C O L L E C T I V E
+      </h3>
       <div className={styles.loginContainer}>
         <h4>LOGIN</h4>
         <form>
           <div className={styles.formGroup}>
             <label>Username</label>
-            <input type="text" className={styles.inputField} placeholder="" />
+            <input
+              type="text"
+              className={styles.inputField}
+              placeholder=""
+            />
           </div>
           <div className={styles.formGroup}>
             <label>Password</label>
-            <input type="password" className={styles.inputField} placeholder="" />
+            <div className={styles.passwordWrapper}>
+              <input
+                type={showPassword ? "text" : "password"}
+                className={styles.inputField}
+                placeholder=""
+              />
+              <span
+                className={styles.eyeIcon}
+                onClick={() => setShowPassword((prev) => !prev)}
+              >
+                {showPassword ? <FaEyeSlash /> : <FaEye />}
+              </span>
+            </div>
           </div>
           <div className={styles.rememberMe}>
             <div>
-              <input className= {styles.checkbox} type="checkbox" /> Remember
+              <input className={styles.checkbox} type="checkbox" /> Remember
             </div>
-            <a className={styles.forgot} href="#">Forgot password</a>
+            <a className={styles.forgot} href="#">
+              Forgot password
+            </a>
           </div>
-          <button type="submit" className={styles.loginBtn}>LOGIN</button>
+          <button type="submit" className={styles.loginBtn}>
+            LOGIN
+          </button>
         </form>
         <p className={styles.orText}>or</p>
         <div className={styles.socialIcons}>
-        <div className={styles.iconCircle}>
+          <div className={styles.iconCircle}>
             <FcGoogle className={styles.googleIcon} />
-          </div> 
+          </div>
           <div className={styles.iconCircle}>
             <FaFacebook className={styles.facebookIcon} />
           </div>
         </div>
         <p className={styles.signupText}>
-          Don't have an account? <a className={styles.sign} href="#">Signup</a>
+          Don't have an account?{" "}
+          <a className={styles.sign} href="#">
+            Signup
+          </a>
         </p>
       </div>
     </div>
